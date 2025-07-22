60e8d5c57b59e1f2de807fa7a3efe75c42863e25
Giancarlo Trinidad <121566221+GiancarloTrinidad@users.noreply.github.com>
2025-07-22 16:35:09 +0800

    notes

--- src/app/(auth)/sign-in/[[...sign-in]]/page.js
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.js
@@ -1,5 +1,8 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+    return (
+        //logo here
+        <SignIn />
+    );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -15,18 +15,18 @@ export default function Home() {
       <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center px-4">
         <div className="max-w-2xl text-center">
           <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-6">
-            Welcome to Your Budget App
+            Welcome to BudgetBerry
           </h1>
           <p className="text-lg md:text-xl text-gray-600 mb-8">
             Plan, track, and achieve your financial goals with ease. Start
             managing your money today in a simple and intuitive way.
           </p>
-          <SignInButton className="cursor-pointer"/>
-             <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                 Sign Up
-                </button>
-              </SignUpButton>
+          <SignInButton className="text-black cursor-pointer px-5"/>
+          <SignUpButton>
+            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton>
           {/* <a
             href="/sign-up"
             className="inline-block bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg shadow-md hover:bg-blue-700 transition-colors duration-300"
