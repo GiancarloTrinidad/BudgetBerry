3b4d98dd9a3273f16b298c8d732cd17a63c13bba
Giancarlo Trinidad <121566221+GiancarloTrinidad@users.noreply.github.com>
2025-07-24 00:24:18 +0800

    logo

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,9 +1,10 @@
+import Logo from '@/components/Logo'
 import React from 'react'
 
 export default function layout({ children }) {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
-        {/* logo here */}
+        <Logo />
         <div className="mt-12">{children}</div>
     </div>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,46 +1,51 @@
+'use client';
+
 import Head from "next/head";
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { 
+  SignInButton, 
+  SignUpButton,
+  SignedIn,
+  SignedOut
+} from "@clerk/nextjs";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/welcome');
+  }, [router]);
+
   return (
     <>
-      <Head>
-        <title>Welcome to Your Budget App</title>
-        <meta
-          name="description"
-          content="Take control of your finances with our simple and intuitive budgeting app."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center px-4">
-        <div className="max-w-2xl text-center">
-          <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-6">
-            Welcome to BudgetBerry
-          </h1>
-          <p className="text-lg md:text-xl text-gray-600 mb-8">
-            Plan, track, and achieve your financial goals with ease. Start
-            managing your money today in a simple and intuitive way.
-          </p>
-          <SignInButton className="text-black cursor-pointer px-5"/>
-          <SignUpButton>
-            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-              Sign Up
-            </button>
-          </SignUpButton>
-          {/* <a
-            href="/sign-up"
-            className="inline-block bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg shadow-md hover:bg-blue-700 transition-colors duration-300"
-          >
-            Sign up
-          </a>
-          <a
-            href="/sign-in"
-            className="inline-block bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg shadow-md hover:bg-blue-700 transition-colors duration-300"
-          >
-            Sign in
-          </a> */}
+      <SignedOut>
+        <Head>
+          <title>Welcome to Your Budget App</title>
+          <meta
+            name="description"
+            content="Take control of your finances with our simple and intuitive budgeting app."
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center px-4">
+          <div className="max-w-2xl text-center">
+            <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-6">
+              Welcome to BudgetBerry
+            </h1>
+            <p className="text-lg md:text-xl text-gray-600 mb-8">
+              Plan, track, and achieve your financial goals with ease. Start
+              managing your money today in a simple and intuitive way.
+            </p>
+            <SignInButton className="text-black cursor-pointer px-5"/>
+            <SignUpButton>
+              <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </div>
         </div>
-      </div>
+      </SignedOut>
     </>
   );
 }
